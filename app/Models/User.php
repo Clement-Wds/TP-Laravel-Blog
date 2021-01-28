9cb38c211bcfa77cd4d90d778c46cba425f583af
Clement-Wds <clementwalshdeserrant@mail.com>
2021-01-28 15:50:37 +0100

    MAJ Database followings / Model User follow

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,23 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts(){
+        return $this->hasMany(Post::class)->latest();
+    }
+
+    //Récupérer tous les abonnements
+    public function followings(){
+        return $this->belongsToMany(User::class, 'followings', 'follower_id', 'following_id');
+    }
+
+    //Récupérer les abonnés
+    public function followers(){
+        return $this->belongsToMany(User::class, 'followings', 'following_id', 'follower_id');
+    }
+
+    //Récupérer les abonnements et vérifier que ces utilisateurs existent (en cas de suppresion de compte)
+    public function follow($user){
+        return $this->followings()->where('following_id', $user->id)->exist();
+    }
 }
